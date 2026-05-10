73b3fd58b1a9e00a6d10dfd9194696c847742083
Ivan Shibin <thejamesmorra@gmail.com>
2026-05-10 19:10:06 +0300

    init commit

---
+++ src/main/java/org/example/sqlbasicsprojectmodule/SqlBasicsProjectModuleApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqlbasicsprojectmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlBasicsProjectModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlBasicsProjectModuleApplication.class, args);
+    }
+
+}
